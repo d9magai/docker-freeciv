a217e6cbb7b7df051e7e5114bd31a5e61015b3f8
d9magai <kumagai@sencorp.co.jp>
2016-03-24 18:52:50 +0900

    unnecessary chmod

--- setup.sh
+++ setup.sh
@@ -5,8 +5,6 @@ yum install -y expect which mariadb
 
 curl -sLO http://svn.gna.org/svn/freeciv/tags/R${FREECIV_MAJOR_VERSION}_${FREECIV_MINOR_VERSION}_${FREECIV_TEENY_VERSION}/scripts/setup_auth_server.sh
 
-chmod +x ./setup_auth_server.sh
-
 ./setup.exp
 
 rm setup_auth_server.sh
